b613f0ff74a37fb51d8910fd33e95bd01e7fe732
Ran-n <ran-n@tutanota.com>
2022-03-11 18:23:24 +0100

    script db finalizado

--- src/modelo/script_db.sql
+++ src/modelo/script_db.sql
@@ -12,7 +12,8 @@ CREATE TABLE IF NOT EXISTS "usuario" (
     "creacion"      TEXT NOT NULL,
     "modificacion"  TEXT,
     "estado"        INTEGER NOT NULL DEFAULT 0,
-    "nome"          TEXT NOT NULL,
+    "nome"          TEXT UNIQUE NOT NULL,
+    "contrasinal"   TEXT,
     CONSTRAINT usuarioPK PRIMARY KEY ("id"),
     CONSTRAINT usuarioFK1 FOREIGN KEY ("estado") REFERENCES "estado"("id") ON DELETE CASCADE ON UPDATE CASCADE MATCH [FULL]
 );
@@ -35,9 +36,11 @@ CREATE TABLE IF NOT EXISTS "elemento" (
     "modificacion"  TEXT,
     "estado"        INTEGER NOT NULL DEFAULT 0,
     "continente"    INTEGER,
+    "id_tipo"       INTEGER NOT NULL,
     CONSTRAINT elementoPK PRIMARY KEY ("id"),
     CONSTRAINT elementoFK1 FOREIGN KEY ("estado") REFERENCES "estado"("id") ON DELETE CASCADE ON UPDATE CASCADE MATCH [FULL],
-    CONSTRAINT elementoFK2 FOREIGN KEY ("continente") REFERENCES "elemento"("id") ON DELETE CASCADE ON UPDATE CASCADE MATCH [FULL]
+    CONSTRAINT elementoFK2 FOREIGN KEY ("continente") REFERENCES "elemento"("id") ON DELETE CASCADE ON UPDATE CASCADE MATCH [FULL],
+    CONSTRAINT elementoFK3 FOREIGN KEY ("id_tipo") REFERENCES "tipo"("id") ON DELETE CASCADE ON UPDATE CASCADE MATCH [FULL]
 );
 
 CREATE TABLE IF NOT EXISTS "opinion" (
@@ -48,8 +51,10 @@ CREATE TABLE IF NOT EXISTS "opinion" (
     "nota"          INTEGER,
     "titulo"        TEXT,
     "contido"       TEXT NOT NULL,
+    "id_elemento"   INTEGER NOT NULL,
     CONSTRAINT opinionPK PRIMARY KEY ("id"),
-    CONSTRAINT opinionFK1 FOREIGN KEY ("estado") REFERENCES "estado"("id") ON DELETE CASCADE ON UPDATE CASCADE MATCH [FULL]
+    CONSTRAINT opinionFK1 FOREIGN KEY ("estado") REFERENCES "estado"("id") ON DELETE CASCADE ON UPDATE CASCADE MATCH [FULL],
+    CONSTRAINT opinionFK2 FOREIGN KEY ("id_elemento") REFERENCES "elemento"("id") ON DELETE CASCADE ON UPDATE CASCADE MATCH [FULL]
 );
 
 CREATE TABLE IF NOT EXISTS "consumicion" (
@@ -58,6 +63,33 @@ CREATE TABLE IF NOT EXISTS "consumicion" (
     "comezo"        TEXT NOT NULL,
     "fin"           TEXT NOT NULL,
     "tempo"         INTEGER NOT NULL,
+    "id_usuario"    INTEGER NOT NULL,
+    "id_elemento"   INTEGER NOT NULL,
     CONSTRAINT consumicionPK PRIMARY KEY ("id"),
-    CONSTRAINT consumicionFK1 FOREIGN KEY ("estado") REFERENCES "estado"("id") ON DELETE CASCADE ON UPDATE CASCADE MATCH [FULL]
+    CONSTRAINT consumicionFK1 FOREIGN KEY ("estado") REFERENCES "estado"("id") ON DELETE CASCADE ON UPDATE CASCADE MATCH [FULL],
+    CONSTRAINT consumicionFK2 FOREIGN KEY ("id_usuario") REFERENCES "usuario"("id") ON DELETE CASCADE ON UPDATE CASCADE MATCH [FULL],
+    CONSTRAINT consumicionFK3 FOREIGN KEY ("id_elemento") REFERENCES "elemento"("id") ON DELETE CASCADE ON UPDATE CASCADE MATCH [FULL]
+);
+
+CREATE TABLE IF NOT EXISTS "rol" (
+    "id"    INTEGER UNIQUE NOT NULL,
+    "nome"  TEXT NOT NULL,
+    CONSTRAINT rolPK PRIMARY KEY ("id")
+);
+
+CREATE TABLE IF NOT EXISTS "autoria" (
+    "id_elemento"   INTEGER NOT NULL,
+    "id_autoria"    INTEGER NOT NULL,
+    "id_rol"        INTEGER NOT NULL,
+    CONSTRAINT autoriaPK PRIMARY KEY ("id_elemento", "id_autoria", "id_rol"),
+    CONSTRAINT autoriaFK1 FOREIGN KEY ("estado") REFERENCES "estado"("id") ON DELETE CASCADE ON UPDATE CASCADE MATCH [FULL],
+    CONSTRAINT autoriaFK2 FOREIGN KEY ("id_usuario") REFERENCES "usuario"("id") ON DELETE CASCADE ON UPDATE CASCADE MATCH [FULL]
+);
+
+CREATE TABLE IF NOT EXISTS "usuario_opinion" (
+    "id_usuario"    INTEGER NOT NULL,
+    "id_opinion"    INTEGER NOT NULL,
+    CONSTRAINT usuario_opinionPK PRIMARY KEY ("id_usuario", "id_opinion"),
+    CONSTRAINT usuario_opinionFK1 FOREIGN KEY ("id_usuario") REFERENCES "usuario"("id") ON DELETE CASCADE ON UPDATE CASCADE MATCH [FULL],
+    CONSTRAINT usuario_opinionFK2 FOREIGN KEY ("id_opinion") REFERENCES "opinion"("id") ON DELETE CASCADE ON UPDATE CASCADE MATCH [FULL]
 );
